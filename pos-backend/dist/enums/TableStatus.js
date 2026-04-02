7f2539cf59e0ac3d36fbd2b30bd82968bd883c3f
Enkhbaatar Idersaikhan <roberthdoll@gmail.com>
2026-04-02 12:19:43 +0100

    Added .gitignore

--- pos-backend/dist/enums/TableStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TableStatus = void 0;
-var TableStatus;
-(function (TableStatus) {
-    TableStatus["OPEN"] = "open";
-    TableStatus["CLOSED"] = "closed";
-})(TableStatus || (exports.TableStatus = TableStatus = {}));
