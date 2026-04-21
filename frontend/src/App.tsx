9a78ccb868d4927f323a53b661162e21c9244dff
Enkhbaatar Idersaikhan <roberthdoll@gmail.com>
2026-04-21 03:50:48 +0100

    Added the image for the login page and set up the login page background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { LoginPage } from "./pages/auth/LoginPage";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold text-indigo-600">Check</h1>;
+  return <LoginPage />;
 }
---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,10 @@
+export function LoginPage() {
+  return (
+    <div
+      className="h-screen w-screen flex"
+      style={{
+        background: "linear-gradient(to right, #628897 27%, #0C2B4E 74%)",
+      }}
+    ></div>
+  );
+}
