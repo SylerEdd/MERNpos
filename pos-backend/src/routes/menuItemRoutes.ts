7d3321e402113bb33468bac9951cd3e16286c39e
Lucas Crema <lucasrcrema@gmail.com>
2026-04-30 10:41:11 +0100

    Added update and delete routes

--- pos-backend/src/routes/menuItemRoutes.ts
+++ pos-backend/src/routes/menuItemRoutes.ts
@@ -7,3 +7,5 @@ const controller = new MenuItemController();
 menuItemRouter.get("/", (req, res) => controller.getAll(req, res));
 menuItemRouter.get("/:id", (req, res) => controller.getById(req, res));
 menuItemRouter.post("/", (req, res) => controller.create(req, res));
+menuItemRouter.put("/:id", (req, res) => controller.update(req, res));
+menuItemRouter.delete("/:id", (req, res) => controller.delete(req, res));
